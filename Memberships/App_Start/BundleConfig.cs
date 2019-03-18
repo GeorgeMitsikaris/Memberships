096aac2df991879484b21c86cad71eb204128427
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-03-18 12:46:47 +0200

    Adding thumbnail.css HttpContextExtensions, ThumbnailsExtensions, ThumbnailModel, ThumbnailAreaModel

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -30,7 +30,8 @@ namespace Memberships
                       "~/Scripts/adminMenu.js"));
 
             bundles.Add(new StyleBundle("~/Content/membership").Include(
-                      "~/Content/navbar.css"));
+                      "~/Content/navbar.css",
+                      "~/Content/thumbnails.css"));
         }
     }
 }
