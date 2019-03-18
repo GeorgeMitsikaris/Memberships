461127f2e504d8baca2c8f9c0dc6400ef29dd825
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-03-18 13:56:09 +0200

    Add GetProductThumbnailsAsync and ThumbnailEqualityComparer

--- Memberships/Extensions/ThumbnailsExtensions.cs
+++ Memberships/Extensions/ThumbnailsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Memberships.Models;
+﻿using Memberships.Comparers;
+using Memberships.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,7 +11,7 @@ namespace Memberships.Extensions
 {
     public static class ThumbnailsExtensions
     {
-        public static async Task<List<int>> GetSubscriptionIdsAsync(string userId=null, ApplicationDbContext db = null)
+        public static async Task<List<int>> GetSubscriptionIdsAsync(string userId = null, ApplicationDbContext db = null)
         {
             try
             {
@@ -26,6 +27,41 @@ namespace Memberships.Extensions
 
             }
             return new List<int>();
+        }
+
+        public static async Task<IEnumerable<ThumbnailModel>> GetProuductThumbnailsAsync(this IEnumerable<ThumbnailModel> thumbnails, string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<ThumbnailModel>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                var subscriptionsIds = await GetSubscriptionIdsAsync(userId, db);
+
+                thumbnails = await (from sp in db.SubscriptionProducts
+                                    join p in db.Products
+                                    on sp.ProductId equals p.Id
+                                    join plt in db.productLinkTexts
+                                    on p.ProductLinkTextId equals plt.Id
+                                    join pt in db.productTypes
+                                    on p.ProductTypeid equals pt.Id
+                                    where subscriptionsIds.Contains(sp.SubscriptionId)
+                                    select new ThumbnailModel
+                                    {
+                                        ProductId = p.Id,
+                                        SubscriptionId = sp.SubscriptionId,
+                                        Title = p.Title,
+                                        Description = p.Description,
+                                        Link = "/ProductContent/Index/" + p.Id,
+                                        ContentTag = pt.Title,
+                                        ImageUrl = p.ImageUrl,
+                                        TagText = plt.Title
+                                    }
+                                   ).ToListAsync();
+
+            }
+            catch { }
+            return thumbnails.Distinct(new ThumbnailEqualityComparer()).OrderBy(t => t.Title);
         }
     }
 }
